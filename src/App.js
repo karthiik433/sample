8b48b0a76963f03f82f8f585cb361d163a3e2ac3
team-10a <karthik12345kumar@gmail.com>
2022-12-11 02:11:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>Hey dude welcome back</h1>
+      <h2>Minor change to folder</h2>
+    </div>
+  );
+}
+
+export default App;
